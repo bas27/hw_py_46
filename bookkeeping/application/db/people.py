3449b0027ea6dc1eab7dfb0c3a29b97e8284d8ba
Alexander B <bas2784@gmail.com>
2022-01-12 14:33:55 +0300

    hw_1_Import.Module.Package

---
+++ bookkeeping/application/db/people.py
@@ -0,0 +1,6 @@
+print('Start get_employees in people.py')
+
+def get_employees():
+    print('get_employees in people.py')
+
+print('End get_employees in people.py')
---
+++ bookkeeping/application/salary.py
@@ -0,0 +1,6 @@
+print('Start calculate_salary')
+
+def calculate_salary():
+    print('calculate_salary')
+
+print('End calculate_salary')
